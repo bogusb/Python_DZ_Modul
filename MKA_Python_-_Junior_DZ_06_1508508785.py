87b43e3c6401e53766103cf4cc24ff251cbc7654
bogusb <barszczbogus@gmail.com>
2022-02-01 00:59:16 +0200

    Przesłuchana lekcja i dodana treść zadania

--- MKA_Python_-_Junior_DZ_06_1508508785.py
+++ MKA_Python_-_Junior_DZ_06_1508508785.py
@@ -26,3 +26,26 @@
 # Если пользователь отвечает [Да], игра начинается заново,
 # если отвечает [Нет] – игра заканчивается.
 
+# # pet project
+import random
+game = True
+money = 0
+while game:
+    secret_number = random.randint(1,20)
+    guess_number = 0
+    lifes = 5
+    while secret_number != guess_number and lifes > 0:
+        guess_number = int(input("Enter number : "))
+        lifes = lifes - 1
+        print(f'You have {lifes} lifes')
+        if secret_number < guess_number:
+            print('Your number greater than secret\nТвоё число больше, чем загаданное')
+        elif secret_number > guess_number:
+            print('Your number less than secret\nТвоё число меньше, чем загаданное')
+    if lifes > 0:
+        print('YOU WIN!!!')
+        money = money + 5
+    else:
+        print('YOU LOSE')
+    print(f'You have {money} money')
+
