269ff85d6ba54fa19ac6edb41bb1c90659eb7313
bogusb <barszczbogus@gmail.com>
2022-02-05 02:56:13 +0200

    5 persons / poprawione

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -7,6 +7,7 @@
 #
 # Курс: Python Junior
 #
+# ------------------------------------------
 # Задание 1
 # Доработайте игру, которую мы делали на уроке.
 # - Добавьте возможность после окончания игры поиграть
@@ -17,13 +18,6 @@
 # просят ввести свой выбор еще раз, пока он не будет
 # правильным.
 #
-# Задание 2
-# (Дополнительное, на высокую оценку)
-# Есть другие варианты игры, которые содержат большее
-# количество вариантов – «Камень, ножницы, бумага, ящерица,
-# Спок». Переделайте игру по такому принципу.
-
-# # ------------------------------------------
 # # Камень Ножницы Бумага
 # # Rock Paper Scissors
 # import random
@@ -57,7 +51,7 @@
 #             if game_type == 'pvp':
 #                 player2_choice = str(input(f"Enter your choice {player2_name}: [r],[p],[s] : "))
 #             elif game_type == 'pve':
-#                 player2_name = random.choice('rps')  # Выбирает 1 символ l - lizard, k - Spock
+#                 player2_choice = random.choice('rps')  # Выбирает 1 символ l - lizard, k - Spock
 #
 #         # Compare data
 #         if player1_choice == player2_choice:
@@ -108,24 +102,25 @@
 # Есть другие варианты игры, которые содержат большее
 # количество вариантов – «Камень, ножницы, бумага, ящерица,
 # Спок». Переделайте игру по такому принципу.
-# ------------------------------------------
+
 # Ножницы Бумага Камень Ящерица Спок
 # Scissors Paper Rock Lizard Spock
-print('Scissors Paper Rock Lizard Spock\n')
-print('''The rules:
- 
-Scissors cuts paper, 
-paper covers rock, 
-rock crushes lizard, 
-lizard poisons Spock, 
-Spock smashes scissors, 
+print('      »»»»»»  Scissors Paper Rock Lizard Spock  ««««««\n')
+print('''      The rules:
 
-scissors decapitates lizard, 
-lizard eats paper, 
-paper disproves Spock, 
-Spock vaporizes rock, 
-    and as it always has, 
-rock crushes scissors.''')
+   «Scissors cuts paper, 
+   paper covers rock, 
+   rock crushes lizard, 
+   lizard poisons Spock, 
+   Spock smashes scissors, 
+
+   scissors decapitates lizard, 
+   lizard eats paper, 
+   paper disproves Spock, 
+   Spock vaporizes rock, 
+       and as it always has, 
+   rock crushes scissors».
+''')
 
 import random
 
@@ -142,10 +137,13 @@ while game:
     player2_score = 0
     player1_name = str(input("Enter your name player 1 : "))
     player2_name = 'Unknown'
-    if game_type == 'pvp':
-        player2_name = str(input("Enter your name player 2 : "))
-    elif game_type == 'pve':
-        player2_name = 'Bot'
+    if game_type == 'pve':
+        player2_name = 'Computer_Bot'
+    elif game_type == 'pvp':
+        player2_name = str(input("Enter your name player 2:  [Bot] = play with computer  : "))
+        if player2_name == 'Bot' or player2_name == 'bot':
+            game_type = 'pve'
+            player2_name = 'Computer_Bot'
     rounds = 3
 
     # Start of game
@@ -156,39 +154,42 @@ while game:
         # Enter data
         while player1_choice != 's' and player1_choice != 'p' and player1_choice != 'r' and player1_choice != 'l' and player1_choice != 'k':
             player1_choice = str(input(f"Enter your choice {player1_name}: [s] [p] [r] [l] [k] : "))
-        while player2_choice != 's' and player2_choice != 'p' and player2_choice != 'r' and player1_choice != 'l' and player1_choice != 'k':
+        while player2_choice != 's' and player2_choice != 'p' and player2_choice != 'r' and player2_choice != 'l' and player2_choice != 'k':
             if game_type == 'pvp':
                 player2_choice = str(input(f"Enter your choice {player2_name}: [s] [p] [r] [l] [k] : "))
             elif game_type == 'pve':
-                player2_name = random.choice('sprlk')  # Выбирает 1 символ l - lizard, k - Spock
+                player2_choice = random.choice('sprlk')  # Выбирает 1 символ l - lizard, k - Spock
+                print(f'{player2_name}  choice is  [{player2_choice}].')
 
         # Compare data
         round_choice = player1_choice + player2_choice
         if player1_choice == player2_choice:
             print('Draw round')
         elif round_choice == 'sp' or round_choice == 'pr' or round_choice == 'rl' or round_choice == 'lk' or round_choice == 'ks':
-            print(f'{player1_name} win the round!')
+            print(f'{player1_name}  win the round!')
             player1_score = player1_score + 1
         elif round_choice == 'sl' or round_choice == 'lp' or round_choice == 'pk' or round_choice == 'kr' or round_choice == 'rs':
-            print(f'{player1_name} win the round!')
+            print(f'{player1_name}  win the round!')
             player1_score = player1_score + 1
         else:
-            print(f'{player2_name} win the round!')
+            print(f'{player2_name}  win the round!')
             player2_score = player2_score + 1
 
     # Compare score
     if player1_score > player2_score:
-        print(f'{player1_name} win the game!')
+        print(f'{player1_name}  win the game!')
         global_player1_score = global_player1_score + 1
     elif player2_score > player1_score:
-        print(f'{player2_name} win the game!')
+        print(f'{player2_name}  win the game!')
         global_player2_score = global_player2_score + 1
     else:
         print('Draw game!')
+    print()
     # Another game?
     # game = bool(input("If you want continue press any key : "))
     another_game = ''
     while another_game != 'y' and another_game != 'n':
-        another_game = str(input(f"Another game?  [y] yes,  [n] no  ? "))
+        another_game = str(input(f"Another game?  [y] yes  [n] no  ? "))
+    print()
     if another_game == 'n':
         game = False
