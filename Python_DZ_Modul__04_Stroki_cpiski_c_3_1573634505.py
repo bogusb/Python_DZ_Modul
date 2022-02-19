f30177db6b8e5e1a640f74bf992055814a56e4be
bogusb <barszczbogus@gmail.com>
2022-02-19 22:13:43 +0200

    Lesson16 ad3

--- Python_DZ_Modul__04_Stroki_cpiski_c_3_1573634505.py
+++ Python_DZ_Modul__04_Stroki_cpiski_c_3_1573634505.py
@@ -26,35 +26,50 @@
 # Variables
 list_of_integers_1 = []
 list_of_integers_2 = []
-list_3 = []
+ad1_list = []
 ad2_list = []
+ad3_list = []
+ad4_list = []
+ad5_list = []
 
+# Original two lists
 import random
 for i in range(12):
     list_of_integers_1.append(random.randint(-30, 30))
 for i in range(8):
     list_of_integers_2.append(random.randint(-20, 20))
 
-# Original two lists
-print('Original:')
-print(f'List 1 ({len(list_of_integers_1)} elements): {list_of_integers_1}')
-print(f'List 2 ({len(list_of_integers_2)} elements): {list_of_integers_2}')
+# ad 1 - Сформировать третий список, содержащий элементы
+# обоих списков;
+ad1_list = list_of_integers_1 + list_of_integers_2
 
-# ad 1 - Сформировать третий список, содержащий элементы обоих списков;
-list_3 = list_of_integers_1 + list_of_integers_2
-print('   - List, containing elements from both lists:')
-print(f'({len(list_3)} elements): {list_3}')
-list_3.sort()
-print(f'Sorted:\n({len(list_3)} elements): {list_3}')
+# ad 2 - Сформировать третий список, содержащий элементы
+# обоих списков без повторений;
+ad2_list = ad1_list[:]
+for i in ad1_list:
+    if ad2_list.count(i) > 1:
+        ad2_list.remove(i)
 
-# ad 2 - Сформировать третий список, содержащий элементы обоих списков без повторений;
-print('   - List containing elements of both lists without repetitions:')
+# ad 3 - Сформировать третий список, содержащий элементы
+# общие для двух списков
+ad3_list1 = list_of_integers_1[:]
+# ad3_list1.sort()
+ad3_list2 = list_of_integers_2[:]
+# ad3_list2.sort()
 
-for i in range(1, len(list_3)+1):
-    if list_3[i] != list_3[i-1]:
-        ad2_list.append(list_3[i-1])
+# ad3_list1 = [-26, -20, -17, -9, -6, -6, 8, 8, 14, 20, 20, 25]
+# ad3_list2 = [-15, -9, -7, -6, -6, -1, 8, -9, 15, 20, 20]
 
-print(f'({len(ad2_list)} elements): {ad2_list}')
+for i in ad3_list1:
+    if ad3_list2.count(i) > 0:
+        ad3_list2.remove(i)
+        ad3_list.append(i)
+
+
+
+
+
+
 
 
 # minimum_element = list_of_integers[0]
@@ -84,4 +99,20 @@ print(f'({len(ad2_list)} elements): {ad2_list}')
 # print(f'Positive counter: {counter_positive}')
 # print(f'Zeros counter: {counter_of_zeros}')
 
+print('Original:')
+print(f'List 1 ({len(list_of_integers_1)} elements): {list_of_integers_1}')
+print(f'List 2 ({len(list_of_integers_2)} elements): {list_of_integers_2}')
+
+print('\nad 1 - List, containing elements from both lists:')
+print(f'({len(ad1_list)} elements): {ad1_list}')
+
+print('\nad 2 - List containing elements of both lists without repetitions:')
+print(f'({len(ad2_list)} elements): {ad2_list}')
+
+print('\nad 3 - List that contains items common to the two lists:')
+print(f'({len(ad3_list)} elements): {ad3_list}')
+
+
 # input()
+
+
